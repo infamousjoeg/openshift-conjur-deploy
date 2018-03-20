8ff5c7b2ed4ad54c34b6e55b346293d074740895
John Tuttle <jtuttle@conjur.net>
2018-03-20 15:16:20 -0400

    move appliance image check to dependency script

--- 0_check_dependencies.sh
+++ 0_check_dependencies.sh
@@ -12,3 +12,9 @@ if [ "$DOCKER_REGISTRY_PATH" = "" ]; then
   echo "You must set DOCKER_REGISTRY_PATH before running this script."
   exit 1
 fi
+
+# Confirms Conjur image is present.
+if [[ "$(docker images -q $CONJUR_DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
+  echo "You must have the Conjur v4 Appliance tagged as $CONJUR_DOCKER_IMAGE in your Docker engine to run this script."
+  exit
+fi
--- 2_build_and_push_containers.sh
+++ 2_build_and_push_containers.sh
@@ -4,11 +4,6 @@ set -eou pipefail
 . config.sh
 . utils.sh
 
-if [[ "$(docker images -q $CONJUR_DOCKER_IMAGE 2> /dev/null)" == "" ]]; then
-  echo "Must have image tagged $CONJUR_DOCKER_IMAGE available in Docker engine to run this script."
-  exit
-fi
-
 docker login -u _ -p $(oc whoami -t) $DOCKER_REGISTRY_PATH
 
 pushd build/conjur_server
