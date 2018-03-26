f86a0a5eb9dfc755b3a7cfca111cc06611f912ac
John Tuttle <jtuttle@conjur.net>
2018-03-26 15:58:39 -0400

    add pods/exec grant

--- 1_create_openshift_project.sh
+++ 1_create_openshift_project.sh
@@ -9,10 +9,13 @@ else
   echo "Creating '$CONJUR_PROJECT_NAME' project."
   oc new-project $CONJUR_PROJECT_NAME
 
+  # TODO: advise customer to create a specific service account instead of using Conjur here.
   # Must run as root to unpack Conjur seed files on standbys for high availability.
-  # TODO: advise customer to create a specific service account instead of using Conjur here
   oc adm policy add-scc-to-user anyuid -z default
 
-  # Must be able to list master/standby pods to update its haproxy config.
-  oc adm policy add-cluster-role-to-user cluster-reader system:serviceaccount:$CONJUR_PROJECT_NAME:default
+  # TODO: replace this grant with more find-grained roles / rolebindings.
+  # This grants the following permissions to the default service-account to do the following:
+  # - get + list on pods
+  # - create + get on pods/exec
+  oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:$CONJUR_PROJECT_NAME:default
 fi
